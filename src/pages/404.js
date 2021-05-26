609bff112e41a5aed6d9453f7786ff459edc6a8c
Anthony Freda <anthonyfreda323@gmail.com>
2021-05-26 13:09:05 -0400

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,20 +5,26 @@ import Seo from "../components/seo";
 // markup
 const NotFoundPage = () => {
   return (
-    <main>
+    <>
       <Seo />
-      <title>Not found</title>
-      <h1>Page not found</h1>
-      <p>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+      <main className="dark:bg-gray-900 bg-gray-100 text-gray-700 dark:text-gray-300 lowercase">
+        <div className="max-w-2xl mx-6 md:ml-36">
+          <div className="min-h-screen flex flex-col justify-center py-12">
+            <h1 className="text-5xl sm:text-7xl">404</h1>
+            <h2 className="text-2xl sm:text-3xl md:text-4xl text-gray-500 dark:text-gray-400 mb-12">
+              Page not found
+            </h2>
+            <Link
+              tabIndex={0}
+              className="text-red-600 dark:text-red-400 text-lg sm:text-xl"
+              to="/"
+            >
+              Go home
+            </Link>
+          </div>
+        </div>
+      </main>
+    </>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,21 @@ import Main from "../components/layout/Main";
 import Seo from "../components/seo";
 import Switch from "../components/Switch";
 
+const DARK_MODE = "darkMode";
+const getDarkMode = () => {
+  const darkMode = localStorage.getItem(DARK_MODE);
+  if (darkMode) {
+    return JSON.parse(darkMode);
+  }
+
+  return true;
+};
+
 const IndexPage = () => {
-  const [darkMode, setDarkMode] = React.useState(true);
+  const [darkMode, setDarkMode] = React.useState(getDarkMode());
   React.useEffect(() => {
     document.querySelector("html").className = darkMode ? "dark" : "";
+    localStorage.setItem(DARK_MODE, darkMode);
   }, [darkMode]);
   return (
     <>
