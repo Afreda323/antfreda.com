e4a4c19214b1bc26872ce23462adf09cae30ef8f
Anthony Freda <anthonyfreda323@gmail.com>
2021-12-30 13:29:57 -0500

    fix footer

--- src/components/seo.js
+++ src/components/seo.js
@@ -39,7 +39,16 @@ const SEO = ({ title, description }) => {
       <meta property="twitter:image" content={seo.image} />
 
       {/* BG */}
-      <meta name="theme-color" content="currentColor" />
+      <meta
+        name="theme-color"
+        media="(prefers-color-scheme: light)"
+        content="#cbd5e1"
+      />
+      <meta
+        name="theme-color"
+        media="(prefers-color-scheme: dark)"
+        content="#0f172a"
+      />
 
       {/* Fonts */}
       <link rel="preconnect" href="https://fonts.gstatic.com" />
